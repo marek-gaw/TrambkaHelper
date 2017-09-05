c553c5627ed8cec93271245d09598d5ab1456f1d
marek-gaw <marek.gawryszewski@outlook.com>
2017-09-05 21:24:35 +0200

    Added basic layout of main screen with working Exit button.

--- app/src/main/java/mgaw/trambkahelper/MainScreenActivity.java
+++ app/src/main/java/mgaw/trambkahelper/MainScreenActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainScreenActivity extends AppCompatActivity {
 
@@ -24,6 +25,15 @@ public class MainScreenActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button buttonExit = (Button) findViewById(R.id.btnExit);
+        buttonExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
             }
         });
     }
